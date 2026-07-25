9bb6d049ce8d1effaa971cfdb32badf4471fa2b0
Ivan Chintov <ivan.chintov@.com>
2026-07-25 13:50:35 +0300

    feat(service): implement BookCatalog gRPC service
    - Add BookCatalogService implementation
    - Inject BookRepository via constructor
    - Implement GetBook unary RPC
    - Return NOT_FOUND when a book does not exist

---
+++ src/main/java/io/github/ivanchintov/bookcatalog/service/BookCatalogService.java
@@ -0,0 +1,37 @@
+package io.github.ivanchintov.bookcatalog.service;
+
+import io.github.ivanchintov.bookcatalog.proto.Book;
+import io.github.ivanchintov.bookcatalog.proto.BookCatalogGrpc;
+import io.github.ivanchintov.bookcatalog.proto.GetBookRequest;
+import io.github.ivanchintov.bookcatalog.repository.BookRepository;
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
+
+import java.util.Optional;
+
+public class BookCatalogService extends BookCatalogGrpc.BookCatalogImplBase {
+
+    private final BookRepository bookRepository;
+
+    public BookCatalogService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void getBook(GetBookRequest request, StreamObserver<Book> responseObserver) {
+        long bookId = request.getId();
+
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        if (book.isPresent()) {
+            responseObserver.onNext(book.get());
+            responseObserver.onCompleted();
+        } else {
+            responseObserver.onError(
+                    Status.NOT_FOUND
+                            .withDescription("Book with ID: " + bookId + " was not found.")
+                            .asRuntimeException()
+            );
+        }
+    }
+}
